48e09c33fb47a7d2957864af9a23f8f67f5dd9f2
Damyan Yordanov <damyan@users.noreply.github.com>
2023-06-15 14:29:48 +0200

    Split into plugins `onmetal` and `ipam`, so we can run FeDHCP in non-k8s environment (#33)

--- plugins/ipam/plugin.go
+++ plugins/ipam/plugin.go
@@ -5,24 +5,45 @@
 package onmetal
 
 import (
+	"fmt"
 	"net"
-	"time"
 
 	"github.com/coredhcp/coredhcp/handler"
 	"github.com/coredhcp/coredhcp/logger"
 	"github.com/coredhcp/coredhcp/plugins"
 	"github.com/insomniacslk/dhcp/dhcpv6"
+
+	"github.com/mdlayher/netx/eui64"
 )
 
 var log = logger.GetLogger("plugins/onmetal")
 
 var Plugin = plugins.Plugin{
-	Name:   "onmetal",
+	Name:   "ipam",
 	Setup6: setup6,
 }
 
+var (
+	k8sClient K8sClient
+)
+
+func parseArgs(args ...string) (string, string, error) {
+	if len(args) != 2 {
+		return "", "", fmt.Errorf("exactly two arguments must be passed to onmetal plugin, got %d", len(args))
+	}
+
+	namespace := args[0]
+	subnet := args[1]
+	return namespace, subnet, nil
+}
+
 func setup6(args ...string) (handler.Handler6, error) {
-	log.Printf("loaded onmetal plugin for DHCPv6.")
+	namespace, subnet, err := parseArgs(args...)
+	if err != nil {
+		return nil, err
+	}
+	k8sClient = NewK8sClient(namespace, subnet)
+	log.Printf("loaded ipam plugin for DHCPv6.")
 	return handler6, nil
 }
 
@@ -36,33 +57,23 @@ func handler6(req, resp dhcpv6.DHCPv6) (dhcpv6.DHCPv6, bool) {
 
 	relayMsg := req.(*dhcpv6.RelayMessage)
 
+	// Retrieve IPv6 prefix and MAC address from IPv6 address
+	_, mac, err := eui64.ParseIP(relayMsg.PeerAddr)
+	if err != nil {
+		log.Errorf("Could not parse peer address: %s", err)
+		return nil, true
+	}
+
 	ipaddr := make(net.IP, len(relayMsg.LinkAddr))
 	copy(ipaddr, relayMsg.LinkAddr)
 	ipaddr[len(ipaddr)-1] += 1
 
-	m, err := req.GetInnerMessage()
+	log.Infof("Generated IP address %s for mac %s", ipaddr.String(), mac.String())
+	err = k8sClient.createIpamIP(ipaddr, mac)
 	if err != nil {
-		log.Errorf("BUG: could not decapsulate: %v", err)
+		log.Errorf("Could not create IPAM IP: %s", err)
 		return nil, true
 	}
 
-	if m.Options.OneIANA() == nil {
-		log.Debug("No address requested")
-		return resp, false
-	}
-
-	resp.AddOption(&dhcpv6.OptIANA{
-		IaId: m.Options.OneIANA().IaId,
-		Options: dhcpv6.IdentityOptions{Options: []dhcpv6.Option{
-			&dhcpv6.OptIAAddress{
-				IPv6Addr:          ipaddr,
-				PreferredLifetime: 24 * time.Hour,
-				ValidLifetime:     24 * time.Hour,
-			},
-		}},
-	})
-
-	//log.Printf("responding with this packet:\n%s", resp.Summary())
-
-	return resp, false
+	return nil, false
 }
