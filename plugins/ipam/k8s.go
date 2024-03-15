233cd72eb9c428d8f8ec9c3aca00d8e92d4eaa57
Damyan Yordanov <damyan@users.noreply.github.com>
2024-03-15 15:53:32 +0100

    Add multi-subnet support for IPAM (#93)
    * Add multi-subnet support for IPAM
    
    Second IPAM parameter is now a comma-separated list of subnet names.
    They are processed one by one, until a matching CIDR is found, then the
    IP address reservation in the corresponding subnet occurs
    
    Fixes https://github.com/ironcore-dev/FeDHCP/issues/89
    
    * Fix reuse

--- plugins/ipam/k8s.go
+++ plugins/ipam/k8s.go
@@ -6,6 +6,7 @@ package ipam
 import (
 	"context"
 	"encoding/hex"
+	"encoding/json"
 	"net"
 	"os"
 	"reflect"
@@ -24,17 +25,17 @@ import (
 )
 
 const (
-	origin = "dhcp"
+	origin = "fedhcp"
 )
 
 type K8sClient struct {
 	Client        client.Client
 	Namespace     string
-	Subnet        string
+	SubnetNames   []string
 	EventRecorder record.EventRecorder
 }
 
-func NewK8sClient(namespace string, subnet string) K8sClient {
+func NewK8sClient(namespace string, subnetNames []string) K8sClient {
 	if err := ipamv1alpha1.AddToScheme(scheme.Scheme); err != nil {
 		log.Fatal("Unable to add registered types ipam to client scheme: ", err)
 	}
@@ -63,7 +64,7 @@ func NewK8sClient(namespace string, subnet string) K8sClient {
 	return K8sClient{
 		Client:        cl,
 		Namespace:     namespace,
-		Subnet:        subnet,
+		SubnetNames:   subnetNames,
 		EventRecorder: recorder,
 	}
 }
@@ -78,64 +79,91 @@ func (k K8sClient) createIpamIP(ipaddr net.IP, mac net.HardwareAddr) error {
 		return err
 	}
 
-	// a lowercase RFC 1123 subdomain must consist of lower case alphanumeric characters, '-' or '.', and
-	// must start and end with an alphanumeric character.
-	// 2001:abcd:abcd::1 will become 2001-abcd-abcd-0000-0000-0000-0000-00001
-	longIpv6 := getLongIPv6(ipaddr)
-	name := longIpv6 + "-" + origin
-	ipamIP := &ipamv1alpha1.IP{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
-			Namespace: k.Namespace,
-			Labels: map[string]string{
-				"ip":     longIpv6,
-				"mac":    macKey,
-				"origin": origin,
+	for _, subnetName := range k.SubnetNames {
+		subnet := &ipamv1alpha1.Subnet{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      subnetName,
+				Namespace: k.Namespace,
 			},
-		},
-		Spec: ipamv1alpha1.IPSpec{
-			IP: ip,
-			Subnet: corev1.LocalObjectReference{
-				Name: k.Subnet,
-			},
-		},
-	}
-
-	existingIpamIP := ipamIP.DeepCopy()
-	err = k.Client.Get(ctx, client.ObjectKeyFromObject(ipamIP), existingIpamIP)
-	if err != nil && !apierrors.IsNotFound(err) {
-		err = errors.Wrapf(err, "Failed to get IP %s in namespace %s", ipamIP.Name, ipamIP.Namespace)
-		return err
-	}
-
-	createIpamIP := false
-	// create IPAM IP if not exists or delete existing if ip differs
-	if apierrors.IsNotFound(err) {
-		createIpamIP = true
-	} else {
-		if !reflect.DeepEqual(ipamIP.Spec, existingIpamIP.Spec) {
-			log.Infof("Delete old IP %s in namespace %s", existingIpamIP.Name, existingIpamIP.Namespace)
-
-			// delete old IP object
-			err = k.Client.Delete(ctx, existingIpamIP)
-			if err != nil {
-				err = errors.Wrapf(err, "Failed to delete IP %s in namespace %s", existingIpamIP.Name, existingIpamIP.Namespace)
-				return err
-			}
-
-			k.EventRecorder.Eventf(existingIpamIP, corev1.EventTypeNormal, "Deleted", "Deleted old IPAM IP")
-			createIpamIP = true
 		}
-	}
+		existingSubnet := subnet.DeepCopy()
+		err = k.Client.Get(ctx, client.ObjectKeyFromObject(subnet), existingSubnet)
+		if err != nil && !apierrors.IsNotFound(err) {
+			err = errors.Wrapf(err, "Failed to get subnet %s in namespace %s", subnet.Name, subnet.Namespace)
+			return err
+		}
+		if apierrors.IsNotFound(err) {
+			log.Infof("Cannot select subnet %s, does not exist", subnetName)
+			continue
+		}
+		if !checkIPv6InCIDR(ipaddr, existingSubnet.Spec.CIDR.String()) {
+			log.Infof("Cannot select subnet %s, CIDR mismatch", subnetName)
+			continue
+		}
+		log.Infof("Selecting subnet %s", subnetName)
 
-	if createIpamIP {
-		err = k.Client.Create(ctx, ipamIP)
-		if err != nil {
-			err = errors.Wrapf(err, "Failed to create IP %s in namespace %s", ipamIP.Name, ipamIP.Namespace)
+		// a lowercase RFC 1123 subdomain must consist of lower case alphanumeric characters, '-' or '.', and
+		// must start and end with an alphanumeric character.
+		// 2001:abcd:abcd::1 will become 2001-abcd-abcd-0000-0000-0000-0000-00001
+		longIpv6 := getLongIPv6(ipaddr)
+		name := longIpv6 + "-" + origin
+		ipamIP := &ipamv1alpha1.IP{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: k.Namespace,
+				Labels: map[string]string{
+					"ip":     longIpv6,
+					"mac":    macKey,
+					"origin": origin,
+				},
+			},
+			Spec: ipamv1alpha1.IPSpec{
+				IP: ip,
+				Subnet: corev1.LocalObjectReference{
+					Name: subnetName,
+				},
+			},
+		}
+
+		existingIpamIP := ipamIP.DeepCopy()
+		err = k.Client.Get(ctx, client.ObjectKeyFromObject(ipamIP), existingIpamIP)
+		if err != nil && !apierrors.IsNotFound(err) {
+			err = errors.Wrapf(err, "Failed to get IP %s in namespace %s", ipamIP.Name, ipamIP.Namespace)
 			return err
 		}
 
-		k.EventRecorder.Eventf(ipamIP, corev1.EventTypeNormal, "Created", "Created IPAM IP")
+		createIpamIP := false
+		// create IPAM IP if not exists or delete existing if ip differs
+		if apierrors.IsNotFound(err) {
+			createIpamIP = true
+		} else {
+			if !reflect.DeepEqual(ipamIP.Spec, existingIpamIP.Spec) {
+				log.Infof("\nOld IP: %v,\nnew IP: %v", prettyFormat(existingIpamIP.Spec), prettyFormat(ipamIP.Spec))
+				log.Infof("Delete old IP %s in namespace %s", existingIpamIP.Name, existingIpamIP.Namespace)
+
+				// delete old IP object
+				err = k.Client.Delete(ctx, existingIpamIP)
+				if err != nil {
+					err = errors.Wrapf(err, "Failed to delete IP %s in namespace %s", existingIpamIP.Name, existingIpamIP.Namespace)
+					return err
+				}
+
+				k.EventRecorder.Eventf(existingIpamIP, corev1.EventTypeNormal, "Deleted", "Deleted old IPAM IP")
+				createIpamIP = true
+			}
+		}
+
+		if createIpamIP {
+			err = k.Client.Create(ctx, ipamIP)
+			if err != nil {
+				err = errors.Wrapf(err, "Failed to create IP %s in namespace %s", ipamIP.Name, ipamIP.Namespace)
+				return err
+			}
+
+			k.EventRecorder.Eventf(ipamIP, corev1.EventTypeNormal, "Created", "Created IPAM IP")
+			break
+		}
+		break
 	}
 
 	return nil
@@ -155,4 +183,27 @@ func getLongIPv6(ip net.IP) string {
 		string(dst[28:])
 
 	return strings.ReplaceAll(longIpv6, ":", "-")
+}
+
+func prettyFormat(ipSpec ipamv1alpha1.IPSpec) string {
+	// Marshal the struct into a JSON string with pretty printing
+	jsonBytes, err := json.MarshalIndent(ipSpec, "", "  ")
+	if err != nil {
+		log.Errorf("Error marshalling JSON: %v", err)
+	}
+
+	// Convert the JSON bytes to a string and print
+	return string(jsonBytes)
+}
+
+func checkIPv6InCIDR(ip net.IP, cidrStr string) bool {
+	// Parse the CIDR string
+	_, cidrNet, err := net.ParseCIDR(cidrStr)
+	if err != nil {
+		log.Errorf("Error parsing CIDR: %v\n", err)
+		return false
+	}
+
+	// Check if the CIDR contains the IP
+	return cidrNet.Contains(ip)
 }
