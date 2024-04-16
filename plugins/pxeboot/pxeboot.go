5fa46dc33d0c9bafd89c9ed9f8ee59cf32f51bdb
Damyan Yordanov <damyan@users.noreply.github.com>
2024-04-16 19:05:04 +0200

    Add IPv4 implemenetation for `pxeboot` plugin (#122)
    * Add IPv4 implemenetation
    
    * Change DHCPv4 options to be pointers

--- plugins/pxeboot/pxeboot.go
+++ plugins/pxeboot/pxeboot.go
@@ -20,6 +20,7 @@ package pxeboot
 
 import (
 	"fmt"
+	"github.com/insomniacslk/dhcp/dhcpv4"
 	"net/url"
 
 	"github.com/coredhcp/coredhcp/handler"
@@ -33,11 +34,13 @@ var log = logger.GetLogger("plugins/pxeboot")
 // Plugin wraps plugin registration information
 var Plugin = plugins.Plugin{
 	Name:   "pxeboot",
+	Setup4: setup4,
 	Setup6: setup6,
 }
 
 var (
-	tftpOption, ipxeOption dhcpv6.Option
+	tftpOption, ipxeOption                                       dhcpv6.Option
+	tftpBootFileOption, tftpServerNameOption, ipxeBootFileOption *dhcpv4.Option
 )
 
 func parseArgs(args ...string) (*url.URL, *url.URL, error) {
@@ -55,6 +58,68 @@ func parseArgs(args ...string) (*url.URL, *url.URL, error) {
 	return tftp, ipxe, nil
 }
 
+func setup4(args ...string) (handler.Handler4, error) {
+	tftp, ipxe, err := parseArgs(args...)
+	if err != nil {
+		return nil, err
+	}
+
+	opt1 := dhcpv4.OptBootFileName(tftp.Path)
+	tftpBootFileOption = &opt1
+
+	opt2 := dhcpv4.OptTFTPServerName(tftp.Host[1:])
+	tftpServerNameOption = &opt2
+
+	opt3 := dhcpv4.OptBootFileName(ipxe.String())
+	ipxeBootFileOption = &opt3
+
+	log.Printf("loaded PXEBOOT plugin for DHCPv4.")
+	return pxeBootHandler4, nil
+}
+
+func pxeBootHandler4(req, resp *dhcpv4.DHCPv4) (*dhcpv4.DHCPv4, bool) {
+	log.Debugf("Received DHCPv4 request: %s", req.Summary())
+
+	if tftpBootFileOption == nil || tftpServerNameOption == nil || ipxeBootFileOption == nil {
+		// nothing to do
+		return resp, true
+	}
+
+	if req.IsOptionRequested(dhcpv4.OptionBootfileName) {
+		var opt, opt2 *dhcpv4.Option
+
+		// if iPXE request
+		if req.GetOneOption(dhcpv4.OptionUserClassInformation) != nil {
+			userClassInfo := req.GetOneOption(dhcpv4.OptionUserClassInformation)
+			log.Debugf("UserClassInformation: %s (%x)", string(userClassInfo), userClassInfo)
+			if len(userClassInfo) >= 4 && string(userClassInfo[0:4]) == "iPXE" {
+				opt = ipxeBootFileOption
+			}
+		} else
+		// if TFTP request
+		if req.GetOneOption(dhcpv4.OptionClassIdentifier) != nil {
+			classID := req.GetOneOption(dhcpv4.OptionClassIdentifier)
+			log.Debugf("ClassIdentifier: %s (%x)", string(classID), classID)
+			if len(classID) >= 19 && string(classID[0:19]) == "PXEClient:Arch:0000" {
+				opt = tftpBootFileOption
+				opt2 = tftpServerNameOption
+			}
+		}
+
+		if opt != nil {
+			resp.Options.Update(*opt)
+			log.Debugf("Added option %s", *opt)
+		}
+		if opt2 != nil {
+			resp.Options.Update(*opt2)
+			log.Debugf("Added option %s", *opt2)
+		}
+	}
+
+	log.Debugf("Sent DHCPv4 response: %s", resp.Summary())
+	return resp, true
+}
+
 func setup6(args ...string) (handler.Handler6, error) {
 	tftp, ipxe, err := parseArgs(args...)
 	if err != nil {
@@ -64,10 +129,12 @@ func setup6(args ...string) (handler.Handler6, error) {
 	ipxeOption = dhcpv6.OptBootFileURL(ipxe.String())
 
 	log.Printf("loaded PXEBOOT plugin for DHCPv6.")
-	return pxebootHandler6, nil
+	return pxeBootHandler6, nil
 }
 
-func pxebootHandler6(req, resp dhcpv6.DHCPv6) (dhcpv6.DHCPv6, bool) {
+func pxeBootHandler6(req, resp dhcpv6.DHCPv6) (dhcpv6.DHCPv6, bool) {
+	log.Debugf("Received DHCPv6 request: %s", req.Summary())
+
 	if tftpOption == nil || ipxeOption == nil {
 		// nothing to do
 		return resp, true
@@ -85,6 +152,7 @@ func pxebootHandler6(req, resp dhcpv6.DHCPv6) (dhcpv6.DHCPv6, bool) {
 		// if TFTP request
 		if decap.GetOneOption(dhcpv6.OptionClientArchType) != nil {
 			optBytes := decap.GetOneOption(dhcpv6.OptionClientArchType).ToBytes()
+			log.Debugf("ClientArchType: %s (%x)", string(optBytes), optBytes)
 			if len(optBytes) == 2 && optBytes[0] == 0 && optBytes[1] == 0x07 {
 				opt = &tftpOption
 			}
@@ -105,5 +173,6 @@ func pxebootHandler6(req, resp dhcpv6.DHCPv6) (dhcpv6.DHCPv6, bool) {
 		}
 	}
 
+	log.Debugf("Sent DHCPv6 response: %s", resp.Summary())
 	return resp, true
 }
