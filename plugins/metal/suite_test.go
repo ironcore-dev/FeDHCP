ded327be311eb7ad9ef277696fde35f820dd8b02
Damyan Yordanov <damyan.yordanov@sap.com>
2024-09-19 12:43:36 +0200

    Make reuse compliant

--- plugins/metal/suite_test.go
+++ plugins/metal/suite_test.go
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and IronCore contributors
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
 
 package metal
 
