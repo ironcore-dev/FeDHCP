0ae085a17d9edbd64ccc4e8b06c339494d33bc39
Damyan Yordanov <damyan@users.noreply.github.com>
2024-11-27 09:10:35 +0100

    Implement new configuration strategy for `IPAM` plugin (#219)
    * Implement new configuration strategy for `IPAM` plugin
    
    * Fix linter issues

---
+++ internal/api/ipam_config.go
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and IronCore contributors
+// SPDX-License-Identifier: MIT
+
+package api
+
+type IPAMConfig struct {
+	Namespace string   `yaml:"namespace"`
+	Subnets   []string `yaml:"subnets"`
+}
