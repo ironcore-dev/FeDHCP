537cf8fb876145451dd4ffba7f02bf777b9863dc
Kapil Kumar <kapilyadava.isa@gmail.com>
2025-01-22 21:56:00 +0530

    Add tests to `bluefield` plugin (#253)
    * Add test to bluefeild plugin
    
    * Fix complaince
    
    ---------
    
    Co-authored-by: kkumar <k.kumar@t-systems.com>

---
+++ plugins/bluefield/suite_test.go
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and IronCore contributors
+// SPDX-License-Identifier: MIT
+
+package bluefield
+
+import (
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+const (
+	pollingInterval      = 50 * time.Millisecond
+	eventuallyTimeout    = 3 * time.Second
+	consistentlyDuration = 1 * time.Second
+)
+
+func TestBluefield(t *testing.T) {
+	SetDefaultConsistentlyPollingInterval(pollingInterval)
+	SetDefaultEventuallyPollingInterval(pollingInterval)
+	SetDefaultEventuallyTimeout(eventuallyTimeout)
+	SetDefaultConsistentlyDuration(consistentlyDuration)
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Bluefield Plugin Suite")
+}
